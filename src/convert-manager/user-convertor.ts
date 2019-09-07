d5da4d7e5ab761383b5e6bcf675c5c8651fce5d0
gengchenhui <gengchenhui@gmail.com>
2019-09-07 16:46:59 +0800

    convertor

---
+++ src/convert-manager/user-convertor.ts
@@ -0,0 +1,53 @@
+import { GrpcQrCode, GrpcQrCodeStatus, GrpcQrCodeLogin } from "../schemas/grpc-schemas";
+import { PadplusQrcode, PadplusQrcodeStatus, PadplusQrcodeLogin } from "../schemas/model-user";
+import { QrcodeStatus, LoginStatus } from "../schemas";
+
+export const convertToQrcode = (input: GrpcQrCode): PadplusQrcode => {
+  try {
+    const result: PadplusQrcode = {
+      qrcodeId: input.qrcodeId,
+      qrcode: input.qrcode,
+    };
+    return result;
+  }
+  catch (err) {
+    console.log(`convert to PadplusQrcode error from input:${JSON.stringify(input)}`);
+    throw err;
+  }
+}
+
+
+export const convertToQrcodeStatus = (input: GrpcQrCodeStatus): PadplusQrcodeStatus => {
+  try {
+    const result: PadplusQrcodeStatus = {
+      headUrl: input.head_url,
+      userName: input.user_name,
+      nickName: input.nick_name,
+      status: QrcodeStatus.Waiting,
+    };
+    return result;
+  }
+  catch (err) {
+    console.log(`convert to PadplusQrcodeStatus error from input:${JSON.stringify(input)}`);
+    throw err;
+  }
+}
+
+
+export const convertToQrcodeLogin = (input: GrpcQrCodeLogin): PadplusQrcodeLogin => {
+  try {
+    const result: PadplusQrcodeLogin = {
+      headImgUrl: input.headImgUrl,
+      nickName: input.nickName,
+      status: LoginStatus.Logined,
+      uin: input.uin,
+      userName: input.userName,
+      verifyFlag: input.verifyFlag,
+    };
+    return result;
+  }
+  catch (err) {
+    console.log(`convert to PadplusQrcodeLogin error from input:${JSON.stringify(input)}`);
+    throw err;
+  }
+}
