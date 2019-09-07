f68e3678cd57cead0ee3d9c7ab182594b5d72863
苏畅 <suchang@suchangdeMacBook-Pro.local>
2019-09-07 20:07:36 +0800

    modify: remove useless function

--- src/server-manager/cache-manager.ts
+++ src/server-manager/cache-manager.ts
@@ -310,23 +310,6 @@ export class CacheManager {
    * --------------------------------
    */
 
-  private parseJSON (payload: any) {
-    log.silly(PRE, `parseJSON(${payload})`)
-    return JSON.parse(payload, (_, v) => {
-      if (
-        v !== null
-        && typeof v === 'object'
-        && 'type' in v
-        && v.type === 'Buffer'
-        && 'data' in v
-        && Array.isArray(v.data)
-      ) {
-        return Buffer.from(v.data)
-      }
-      return v
-    })
-  }
-
   private async initCache (
     userId: string,
   ): Promise<void> {
