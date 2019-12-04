d912f3fbe2da7197b935e42612b5776119453e42
su-chang <su_chang@yeah.net>
2019-12-04 13:50:12 +0800

    modify: add reconnect for auto login.

--- src/padplus-manager/api-request/user.ts
+++ src/padplus-manager/api-request/user.ts
@@ -2,6 +2,7 @@ import { RequestClient } from './request'
 import { log } from '../../config'
 import { ApiType } from '../../server-manager/proto-ts/PadPlusServer_pb'
 
+const PRE = 'USER'
 export class PadplusUser {
 
   private requestClient: RequestClient
@@ -13,6 +14,7 @@ export class PadplusUser {
 
   // 初始化登录信息
   public async initInstance () {
+    log.silly(PRE, `initInstance()`)
     await this.requestClient.request({
       apiType: ApiType.INIT,
     })
@@ -20,6 +22,7 @@ export class PadplusUser {
 
   // grpc server重连
   public async reconnect () {
+    log.silly(PRE, `reconnect()`)
     await this.requestClient.request({
       apiType: ApiType.RECONNECT,
     })
@@ -42,95 +45,5 @@ export class PadplusUser {
       return res
     }
   }
-
-  /*
-  // 登出微信
-  public logoutWeChat = async (account: string): Promise<RequestStatus> => {
-    log.silly(PRE, `logoutWeChat(${account})`)
-
-    const data = {
-      my_account: account,
-    }
-
-    const res = await this.requestClient.request({
-      apiName: 'logoutWeixin',
-      data,
-    })
-    log.silly(PRE, `res : ${JSON.stringify(res)}`)
-
-    if (res.code === RequestStatus.Success) {
-      return RequestStatus.Success
-    } else {
-      throw new Error('logout wechat failed.')
-    }
-  }
-
-  // 添加好友
-  public addFriend = async (
-    loginedId: string,
-    account: string,
-    content: string,
-  ): Promise<RequestStatus> => {
-    log.silly(PRE, `addFriend(${loginedId}, ${account}, ${content})`)
-
-    const data = {
-      account: account,
-      content: content,
-      my_account: loginedId,
-    }
-
-    const res = await this.requestClient.request({
-      apiName: 'addFriend',
-      data,
-    })
-    log.silly(PRE, `res : ${JSON.stringify(res)}`)
-    if (res.code === RequestStatus.Success) {
-      return RequestStatus.Success
-    } else {
-      return RequestStatus.Fail
-    }
-  }
-
-  // 删除好友
-  public delFriend = async (loginedId: string, account: string): Promise<RequestStatus> => {
-    log.silly(PRE, `delFriend(${loginedId}, ${account})`)
-
-    const data = {
-      my_account: loginedId,
-      to_account: account,
-    }
-
-    const res = await this.requestClient.request({
-      apiName: 'delFriend',
-      data,
-    })
-    log.silly(PRE, `res : ${JSON.stringify(res)}`)
-    if (res.code === RequestStatus.Success) {
-      return RequestStatus.Success
-    } else {
-      return RequestStatus.Fail
-    }
-  }
-
-  // 自动通过好友
-  public acceptFriend = async (loginedId: string, account: string): Promise<RequestStatus> => {
-    log.silly(PRE, `acceptFriend(${loginedId}, ${account})`)
-
-    const data = {
-      account,
-      my_account: loginedId,
-    }
-
-    const res = await this.requestClient.request({
-      apiName: 'acceptFriend',
-      data,
-    })
-    log.silly(PRE, `res : ${JSON.stringify(res)}`)
-    if (res.code === RequestStatus.Success) {
-      return RequestStatus.Success
-    } else {
-      return RequestStatus.Fail
-    }
-  } */
 
 }
