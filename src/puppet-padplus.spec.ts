b21a0f239aa8bb09740b3be2ed4bf4b297b98078
Huan LI <zixia@zixia.net>
2020-03-18 23:03:06 +0800

    add integration test for restart

---
+++ src/puppet-padplus.spec.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env ts-node
+
+import { test } from 'tstest'
+
+import { PuppetPadplus } from './puppet-padplus'
+
+test('PuppetPlus restart without problem', async (t) => {
+  const puppet = new PuppetPadplus()
+  try {
+    for (let i = 0; i < 3; i++) {
+      await puppet.start()
+      await puppet.stop()
+      t.pass('start/stop-ed at #' + i)
+    }
+    t.pass('PuppetPadplus() start/restart successed.')
+  } catch (e) {
+    t.fail(e)
+  }
+})
