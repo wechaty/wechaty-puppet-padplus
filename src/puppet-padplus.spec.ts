05824caa7e2e26bde3cd9fd851d147b65c9794a2
Huan LI <zixia@zixia.net>
2020-03-18 23:48:43 +0800

    Reproduce the problem (https://github.com/wechaty/wechaty-puppet-padplus/issues/189#issuecomment-600704543)

--- src/puppet-padplus.spec.ts
+++ src/puppet-padplus.spec.ts
@@ -3,9 +3,18 @@
 import { test } from 'tstest'
 
 import { PuppetPadplus } from './puppet-padplus'
+import { MemoryCard } from 'memory-card'
 
 test('PuppetPlus restart without problem', async (t) => {
+  const memory = new MemoryCard()
+  await memory.load()
+  await memory.set('WECHATY_PUPPET_PADPLUS', {
+    uin: true,
+  })
+
   const puppet = new PuppetPadplus()
+  puppet.setMemory(memory)
+
   try {
     for (let i = 0; i < 3; i++) {
       await puppet.start()
