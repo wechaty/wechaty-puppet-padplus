a33ba3f9c89a3a931424893ad6226d9d0eb86246
su-chang <su_chang@yeah.net>
2020-03-31 19:18:23 +0800

    fix: resolve promise of start method, #189

---
+++ src/puppet-padplus.spec.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env ts-node
+
+import { test } from 'tstest'
+
+import { PuppetPadplus } from './puppet-padplus'
+import { MemoryCard } from 'memory-card'
+
+test('PuppetPlus restart without problem', async (t) => {
+  const memory = new MemoryCard()
+  await memory.load()
+  await memory.set('WECHATY_PUPPET_PADPLUS', {
+    uin: true,
+  })
+  const token = 'puppet_padplus_5f6f30b388ca0401'
+  const puppet = new PuppetPadplus({
+    token,
+  })
+  puppet.setMemory(memory)
+
+  try {
+    for (let i = 0; i < 3; i++) {
+      await puppet.start()
+      await puppet.stop()
+      t.pass('start/stop-ed at #' + i)
+    }
+    t.pass('PuppetPadplus() start/restart successed.')
+  } catch (e) {
+    t.fail(e)
+  }
+
+})
