0ac7a43a61e13ded0cffa283a3183505f92142bf
suchang <su_chang@yeah.net>
2023-03-28 22:54:23 +0800

    feat: :sparkles: add @ffmpeg-installer/ffmpeg to config ffmpeg path for ffmpeg-extract-frames

--- src/pure-function-helpers/video-process.ts
+++ src/pure-function-helpers/video-process.ts
@@ -1,13 +1,17 @@
 import { FileBox } from 'file-box'
 import { VideoContent } from '../schemas'
 import { PadplusMessage } from '../padplus-manager/api-request/message'
+import { log } from '../config'
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 const extractFrames = require('ffmpeg-extract-frames')
 const probe = require('ffmpeg-probe')
 
 export async function videoPreProcess (request: PadplusMessage, url: string): Promise<VideoContent> {
   const name = `screenshot-${Date.now()}.jpg`
   const path = './screenshot/' + name
+  log.info(`videoPreProcess ffmpegPath: ${ffmpegPath}`)
   await extractFrames({
+    ffmpegPath,
     input: url,
     offsets: [1],
     output: path,
